d8306bf5f2978a171b832b15277e9044d2c02c08
Coda <cody.koelliker@outlook.com>
2022-02-23 18:43:01 -0700

    Initial commit

---
+++ src/main/java/coda/roasted/Roasted.java
@@ -0,0 +1,29 @@
+package coda.roasted;
+
+import coda.roasted.init.RoastedItems;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mod(Roasted.MOD_ID)
+public class Roasted {
+    public static final String MOD_ID = "roasted";
+    public static final List<Runnable> CALLBACKS = new ArrayList<>();
+
+    public Roasted() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        RoastedItems.ITEMS.register(bus);
+
+        bus.addListener(this::registerClient);
+    }
+
+    private void registerClient(FMLClientSetupEvent event) {
+        CALLBACKS.forEach(Runnable::run);
+        CALLBACKS.clear();
+    }
+}
